3b58f20c068de14cfd6c90ceb216313c3558e324
willbigas <williambmauro@hotmail.com>
2018-12-05 10:38:09 -0200

    Classes Reformuladas

---
+++ src/br/com/automovel/entidade/Automovel.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.automovel.entidade;
+
+/**
+ *
+ * @author Alunos
+ */
+public class Automovel {
+   
+    private String modelo;
+    private String marca;
+    private String placa;
+    private Integer ano;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    @Override
+    public String toString() {
+        return this.modelo + " (" + this.marca + ")";
+    }
+}
